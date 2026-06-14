967fe6e5f7f0613ab06dc80de9ea38c9b8247607
EthanZahner <eezahner2@mail.fhsu.edu>
2026-06-14 18:51:12 -0500

    added edge detection functions to part3.py

--- OneDrive - Wichita State University/Summer26/Homework1/src/part3.py
+++ OneDrive - Wichita State University/Summer26/Homework1/src/part3.py
@@ -79,3 +79,77 @@ def create_four_subsets(image_paths):
                 file.write(str(path) + "\n")
 
     return subsets
+
+# edge detection methods rely on brightness, not color, so it makes sense to convert them to grayscale first
+# then we have to reconvert the edge result back into normal image values for saving
+def convert_to_gray(image):
+    # if image is already grayscale, return it
+    if len(image.shape) == 2:
+        return image
+
+    #convert BGR image to grayscale
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+def normalize_to_uint8(image):
+    # Convert edge result into normal 0-255 image values
+    normalized = cv2.normalize(image, None, 0, 255, cv2.NORM_MINMAX)
+    return normalized.astype(np.uint8)
+
+
+def sobel_edge(gray):
+    # Detect horizontal edges
+    sobel_x = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=3)
+
+    # Detect vertical edges
+    sobel_y = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=3)
+
+    # Combine both edges
+    magnitude = np.sqrt(sobel_x ** 2 + sobel_y ** 2)
+
+    return normalize_to_uint8(magnitude)
+
+
+def laplacian_edge(gray):
+    # detect edges based on quick brightness changes
+    laplacian = cv2.Laplacian(gray, cv2.CV_64F, ksize=3)
+
+    return normalize_to_uint8(np.absolute(laplacian))
+
+def canny_edge(gray):
+    # Use the image median to choose Canny thresholds
+    median = np.median(gray)
+
+    lower = int(max(0, 0.67 * median))
+    upper = int(min(255, 1.33 * median))
+
+    # Backup threshholds if the automatic values are bad
+    if lower == upper:
+        lower = 50
+        upper = 150
+
+    return cv2.Canny(gray, lower, upper)
+
+
+def prewitt_edge(gray):
+    #Prewitt horizontal edge kernel
+    kernel_x = np.array([
+        [-1, 0, 1],
+        [-1, 0, 1],
+        [-1, 0, 1]
+    ], dtype=np.float32)
+
+    # Prewitt vertical edge kernel
+    kernel_y = np.array([
+        [1, 1, 1],
+        [0, 0, 0],
+        [-1, -1, -1]
+    ], dtype=np.float32)
+
+    #  Apply Prewitt filters
+    prewitt_x = cv2.filter2D(gray, cv2.CV_64F, kernel_x)
+    prewitt_y = cv2.filter2D(gray, cv2.CV_64F, kernel_y)
+
+    # Combine both directions
+    magnitude = np.sqrt(prewitt_x ** 2 + prewitt_y ** 2)
+
+    return normalize_to_uint8(magnitude)
